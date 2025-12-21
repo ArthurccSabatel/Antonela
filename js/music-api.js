a9fd91c8810d54d397ad0e1e324956a073214f41
rhudsu <hudsonb994@gmail.com>
2025-12-21 09:59:08 -0400

    feat: estrutura inicial classe album

---
+++ js/music-api.js
@@ -0,0 +1,49 @@
+const urlApi = "https://api.deezer.com/"
+
+
+getAlbums('eminem')
+
+class album {
+    id;
+    nome;
+    cover;
+    genero;
+    lancamento;
+    musicas = []
+
+    constructor(id, nome, genero, lancamento) {
+        this.id = id;
+        this.nome = nome;
+        this.cover = cover;
+        this.genero = genero;
+        this.lancamento =lancamento;
+    }
+
+    addMusica(musica){
+        this.musicas.push(musica)
+    }
+
+
+}
+
+
+
+async function getAlbums(artist) {
+    response = await fetch(`${urlApi}search?q=${artist}&output=json`)
+
+    if(!response.ok)
+        return console.log("not ok")
+
+    response = await response.json()
+
+    response.data.forEach(async album => {
+        albumResponse = await fetch(album.tracklist)
+
+    });
+}
+
+
+
+
+
+
