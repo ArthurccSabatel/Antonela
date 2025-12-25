8d3e59b22f487c92359e2691003333d07bb2d552
rhudsu <hudsonb994@gmail.com>
2025-12-25 03:41:37 -0400

    feat: criação do buscador de musica random que retorna musica e seu album

---
+++ js/quiz-musica.js
@@ -0,0 +1,29 @@
+iniciar()
+
+async function iniciar() {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    adivinheAlbum();
+}
+ 
+function adivinheAlbum(artist = -1){
+    var albuns;
+    
+    if(artist == -1)
+        albuns = Artista.artistaLista[getRandom(Artista.artistaLista)].album;
+    else   
+        albuns = getArtistById(artist).album;
+
+    let {album, musica} = getRandomMusic(albuns);
+    console.log(album, musica)
+ }
+
+
+function getRandom(lista){
+    return Math.floor(Math.random() * lista.length)
+}
+
+function getRandomMusic(albunslist){
+    album = albunslist[getRandom(albunslist)]
+    musica = album.musicas[getRandom(album.musicas)]
+    return {album, musica}
+}
